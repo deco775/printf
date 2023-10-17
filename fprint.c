1e5f382ca356d0c20a30693b3221c733f997ac7f
deco775 <mkansipitso@gmail.com>
2023-10-16 23:51:54 -0700

    printf project

--- fprint.c
+++ fprint.c
@@ -2,51 +2,53 @@
 
 int _printf(const char *format, ...)
 {
-    va_list argList;
-    int printChar = 0;
+	va_list argList;
+	char ch = va_arg(argList, int);
+	char *strng = va_arg(argList, char*);
+	int printChar = 0;
 
-    if(format == NULL)
-    {
-        return -1;
-    }
+	if (format == NULL)
+	{
+	return (-1);
+	}
 
-    va_start(argList, format);
+	va_start(argList, format);
 
-    while(*format)
-    {
-        switch (*format)
-        {
-        case '%': format++;
-                switch (*format)
-                {
-                case '\0': 
-                    break;
-                case '%': write(1, format, 1);
-                    printChar ++;
-                    break;
-                case 'c': char ch = va_arg(argList, int);
-                    write(1, &ch, 1);
-                    printChar++;
-                    break;
-                case 's': char *strng = va_arg(argList, char*);
-                    write(1, strng, strlen(strng));
-                    printChar += strlen(strng);
-                    break;
-                
-                default:
-                    break;
-                }
-            break;
-        
-        default: write(1, format, 1);
-            printChar++;
-            break;
-        }
-
-        format++;
-    }
+	while (*format)
+	{
+	switch (*format)
+	{
+	case '%':
+		format++;
+		switch (*format)
+		{
+		case '\0': 
+			break;
+		case '%':
+			write(1, format, 1);
+			printChar ++;
+			break;
+		case 'c':
+			write(1, &ch, 1);
+			printChar++;
+			break;
+			case 's':
+			write(1, strng, strlen(strng));
+			printChar += strlen(strng);
+			break;
+               	default:
+			break;
+		}
+		break;
+	default: 
+		write(1, format, 1);
+		printChar++;
+		break;
+	}
+	format++;
+	}
     
-    va_end(argList);
+	va_end(argList);
     
-    return printChar;
+	return (printChar);
 }
