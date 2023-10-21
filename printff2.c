b3477ac74343bdf9fdbaa896290c6344bd62333a
deco775 <mkansipitso@gmail.com>
2023-10-20 18:34:09 -0700

    printf project

--- printff2.c
+++ printff2.c
@@ -1,39 +1,57 @@
 #include "main.h"
+/**
+ * _printf - duplicate the printf funtion
+ * format == c - handles the %c format
+ * format == s - handles the %s format
+ * format == % - handles the double %
+ *
+ * Return: the number of charecters printed
+ */
 
 int _printf(const char *format, ...) {
     int charC = 0;
     va_list argL;
+    char *stng = va_arg(argL, char*);
+    int strLen = 0;
 
     if (format == NULL)
         return (-1);
 
     va_start(argL, format);
 
-    while (*format) {
-        if (*format != '%') {
+    while (*format)
+    {
+        if (*format != '%')
+       	{
            _putchar(*format);
             charC++;
-        } else {
+        }
+       	else
+       	{
             format++;
             if (*format == '\0')
             {
                 break;
-            }else if (*format == 's') {
-                char *string = va_arg(argL, char*);
-                int strLen = 0;
+            }
+	    else if (*format == 's')
+	    {
 
-                while (string[strLen] != '\0') {
-                    _putchar(string[strLen]);
+                while (stng[strLen] != '\0')
+	       	{
+                    _putchar(stng[strLen]);
                     strLen++;
                     charC++;
                 }
             }
-            if (*format == '%') {
+            if (*format == '%')
+	    {
                 _putchar('%');
                 charC++;
-            } else if (*format == 'c') {
-                char character = va_arg(argL, int);
-                _putchar(character);
+            }
+	    else if (*format == 'c')
+	    {
+                char cha = va_arg(argL, int);
+                _putchar(cha);
                 charC++;
             } 
         }
